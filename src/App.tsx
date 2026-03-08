185612f656b05c25a2bf9ae258f5b3db2165bdaf
Felipe Medeiros <felipeff_freitas@outlook.com>
2026-03-07 21:04:52 -0300

    chore: Instalando shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
     return (
         <>
             <div>
-                <p className="text-black">Click on the Vite and React logos to learn more</p>
+                <h1 className="text-3xl">Testando shadcn/ui</h1>
+                <p className="text-black">Apenas um teste</p>
+                <Button className="bg-blue-500 text-white hover:bg-blue-600">Click me</Button>
             </div>
         </>
     );
